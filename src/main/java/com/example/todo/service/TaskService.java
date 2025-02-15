86c4c8455927b297e65ae9fccac96fe896ec3c57
mara <mara@MacBook-Air-Marzan.local>
2025-02-16 02:33:58 +0500

    Доработка TaskService: добавлен новый метод

--- src/main/java/com/example/todo/service/TaskService.java
+++ src/main/java/com/example/todo/service/TaskService.java
@@ -1,9 +1,12 @@
+// Оптимизация сервиса задач
 package com.example.todo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.todo.repository.TaskRepository;
 import java.util.List;
+
+
 
 @Service
 public class TaskService {
